2254fa45f612ffb6e98d19a2d188361d08acc1a4
PaingHeinKyaw <paingheink003@gmail.com>
2026-09-05 20:12:02 +0630

    add factory classes for db seeding with faker

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -28,8 +28,8 @@ class UserFactory extends Factory
             'name' => fake()->name(),
             'email' => fake()->unique()->safeEmail(),
             'email_verified_at' => now(),
-            'password' => static::$password ??= Hash::make('password'),
-            'remember_token' => Str::random(10),
+            'password' => static::$password ??= Hash::make('admin123'),
+            // 'remember_token' => Str::random(10),
         ];
     }
 
